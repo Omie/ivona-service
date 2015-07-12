33109f05ee4a76d0772c8fc11cca8fed4b1dc7d7
omie <intelomkar@gmail.com>
2015-07-13 00:34:03 +0530

    one that works

---
+++ ivona.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	ivonago "github.com/jpadilla/ivona-go"
+)
+
+var client *ivonago.Ivona
+
+func initIvona(accessKey, secretKey string) {
+	client = ivonago.New(accessKey, secretKey)
+}
+
+func GetTTS(text, voice string) (resp []byte, err error) {
+	log.Println("--- GetTTS", text, voice)
+
+	options := ivonago.NewSpeechOptions(text)
+	r, err := client.CreateSpeech(options)
+	if err != nil {
+		log.Println("Error getting response from Ivona: text:", err)
+		return
+	}
+
+	return r.Audio, err
+}
+
+func GetVoices(name, language, gender string) (resp []byte, err error) {
+	log.Println("--- GetVoices", name, language, gender)
+
+	options := ivonago.Voice{name, language, gender}
+	r, err := client.ListVoices(options)
+	if err != nil {
+		log.Println("Error getting response from Ivona: text:", err)
+		return
+	}
+
+	js, err := json.Marshal(r)
+	if err != nil {
+		return
+	}
+
+	return js, err
+}
+
+func getTTSHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("--- getTTSHandler")
+	r.ParseForm()
+	text := r.FormValue("text")
+	voice := r.FormValue("voice")
+
+	tts, err := GetTTS(text, voice)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "audio/mpeg3")
+	w.Write(tts)
+}
+
+func getVoicesHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("--- getVoicesHandler")
+	r.ParseForm()
+	name := r.FormValue("name")
+	language := r.FormValue("language")
+	gender := r.FormValue("gender")
+
+	voices, err := GetVoices(name, language, gender)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(voices)
+}
+
+func StartHTTPServer(host string, port string) error {
+	r := mux.NewRouter()
+	r.StrictSlash(true)
+
+	r.HandleFunc("/", getTTSHandler).Methods("POST")
+	r.HandleFunc("/voices", getVoicesHandler).Methods("POST")
+
+	http.Handle("/", r)
+
+	bind := fmt.Sprintf("%s:%s", host, port)
+	log.Println("listening on:", bind)
+	return http.ListenAndServe(bind, nil)
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+
+	accessKey := os.Getenv("IVONA_ACCESSKEY")
+	secretKey := os.Getenv("IVONA_SECRETKEY")
+	if accessKey == "" || secretKey == "" {
+		log.Println("main: ivona credentials not set")
+		return
+	}
+
+	host := os.Getenv("IVONA_SERVICE_HOST")
+	port := os.Getenv("IVONA_SERVICE_PORT")
+	if host == "" || port == "" {
+		log.Println("main: host or port not set")
+		return
+	}
+	initIvona(accessKey, secretKey)
+
+	err := StartHTTPServer(host, port)
+	if err != nil {
+		log.Println("Error starting server", err)
+	}
+
+}
